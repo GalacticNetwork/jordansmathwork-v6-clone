8a5c1b75d24bab08d8c524254767cb9bdec0d5ff
Jordan e <brawlstarrer6@gmail.com>
2025-04-09 06:37:04 +0000

    yes

--- assets/scripts/panic.js
+++ assets/scripts/panic.js
@@ -2,7 +2,7 @@ if (localStorage.getItem("theme") != null) {
   document.body.setAttribute("theme", localStorage.getItem("theme"))
 }
 document.addEventListener("keydown", async (e) => {
-  if (e.key == "`") window.parent.window.location.replace(localStorage.getItem("panicurl") || 'https://classroom.google.com/h')
+  if (e.key == "!") window.parent.window.location.replace(localStorage.getItem("panicurl") || 'https://classroom.google.com/h')
 })
 function panicFunction() {
     location.replace('https://classroom.google.com/h');
