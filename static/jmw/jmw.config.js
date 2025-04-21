f17f9cde4d326820573cb9b0e0b443bd300c81fe
Jordan e <brawlstarrer6@gmail.com>
2025-04-21 16:27:57 +0000

    i

--- static/jmw/jmw.config.js
+++ static/jmw/jmw.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: '/static/jordansmath/work/',
-  bare: 'https://ghost.galacticnetwork.org/bare/',
+  bare: 'https://petezahserver.faizinternational.com.np/bare/',
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: '/static/jmw/jmw.handler.js',
